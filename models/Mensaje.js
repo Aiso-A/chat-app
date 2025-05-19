e04ca44249ea6fc5c58981aa8f9864881a39d538
Ami <amy1511029@gmail.com>
2025-05-18 23:36:46 -0600

    Cambio en mensajes y chat

--- models/Mensaje.js
+++ models/Mensaje.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const mensajeSchema = new mongoose.Schema({
   chat: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
-  texto: { type: String, required: true },
+  texto: { type: String, required: function() { return !this.archivoUrl; } }, 
+  archivoUrl: { type: String }, 
   fecha: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Mensaje', mensajeSchema);
+
+
